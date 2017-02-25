ed7082171ccd0aaec02b01b7066a126cfd34f1d9
Malejazt <maleja1997zt@gmail.com>
2017-02-24 23:11:18 -0500

    Update OrganizarNumeros.java

--- src/organizarnumeros/OrganizarNumeros.java
+++ src/organizarnumeros/OrganizarNumeros.java
@@ -5,19 +5,58 @@
  */
 package organizarnumeros;
 
+import java.util.Scanner;
+
+
+
 /**
  *
- * @author srgab
+ * @author Alejandra
  */
 public class OrganizarNumeros {
+
+  
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       Scanner usuario = new Scanner (System.in);
         
-        System.out.println("Hola Mundo");
-        // TODO code application logic here
-    }
+        int vector [] = new int[10];
+        int aux [] = new int[10];
+        int i,j,orden,cont = 0;
+        
+        for(i = 0 ; i < 10 ; i++){
+            cont=cont+1;
+            System.out.println("Digite el #"+cont);
+            vector[i] = usuario.nextInt();  
+        }
+          for(i = 0 ; i < 10 ; i++){
+           aux[i] =vector[i]; 
+        }
+          
+         for (i=0;i<10;i++){
+	    for(j=i+1;j<10;j++)	{
+	       if (aux[i]>aux[j]){
+	       	
+	       	orden=aux[j];
+	       	aux[j]=aux[i];
+	       	aux[i]=orden;
+	       	
+		   }
+	    }
+         }
+        
+        System.out.print("El orden es:");
+       for (i=0;i<11;i++){
+		   if (aux[i]!=aux[i+1]){
+		   	
+		   		System.out.println(aux[i]+",");
+		   	
+		   }		
+		
+	}	
     
+}
 }
